112c54de22b2fe7639a6ca47533d8ee84ddf942b
ReeSkaaa <160928530+ReeSkaaa@users.noreply.github.com>
2026-06-16 12:52:48 +0300

    feature(libs-qdrant): add qdrant client.py

--- libs/qdrant/client.py
+++ libs/qdrant/client.py
@@ -1,1 +1,23 @@
 # TODO: Реализовать фабрику Qdrant-клиента.
+from typing import Any, Dict, Optional
+from qdrant_client import AsyncQdrantClient
+
+
+class QdrantClientFactory:
+    def __init__(self, config: Dict[str, Any]):
+        # ищем qdrant в конфиге
+        self._cfg = config.get("qdrant", {})
+
+        # параметры подключения
+        self._host = self._cfg.get("host", "localhost")
+        self._port = self._cfg.get("port")
+        self._api_key = self._cfg.get("api_key")
+        self._timeout = self._cfg.get("timeout", 10)  # максимальное время ожидания ответа от сервера
+
+    def create_async_client(self) -> AsyncQdrantClient:
+        return AsyncQdrantClient(
+            host=self._host,
+            port=self._port,
+            api_key=self._api_key,
+            timeout=self._timeout
+        )
