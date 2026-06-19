5964ea4b3c6a78ed20666c63a01e267e3e2ee101
ReeSkaaa <160928530+ReeSkaaa@users.noreply.github.com>
2026-06-19 15:53:47 +0300

    feature(message-service-moderation_module): add step.py

--- services/message_service/moderation_module/step.py
+++ services/message_service/moderation_module/step.py
@@ -1,1 +1,10 @@
 # TODO: Описать интерфейс шага модерации.
+from abc import ABC, abstractmethod
+from typing import Optional
+
+
+# задает только шаблон для других классов
+class ModerationStep(ABC):
+    @abstractmethod
+    async def process(self, text: str) -> Optional[str]:
+        pass
