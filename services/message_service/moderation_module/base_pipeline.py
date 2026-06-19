c10b7fbde8cb3cc86c146aaf3fb6b1695558d738
ReeSkaaa <160928530+ReeSkaaa@users.noreply.github.com>
2026-06-19 17:26:58 +0300

    feature(message-service-moderation_module-base-pipeline): add base_pipeline.py

--- services/message_service/moderation_module/base_pipeline.py
+++ services/message_service/moderation_module/base_pipeline.py
@@ -1,1 +1,8 @@
 # TODO: Реализовать базовый pipeline модерации.
+from typing import List
+from .step import ModerationStep
+
+
+class BaseModerationPipline:
+    def __init__(self, steps: List[ModerationStep]):
+        self.steps = steps
