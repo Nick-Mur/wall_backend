b84f483fcac3cc4827327293a53c8cfd94a87fb4
ReeSkaaa <160928530+ReeSkaaa@users.noreply.github.com>
2026-06-19 17:05:07 +0300

    fix(message-service-moderation_module-base-pipeline): add domain moderation.py dependency

--- services/message_service/moderation_module/step.py
+++ services/message_service/moderation_module/step.py
@@ -1,10 +1,12 @@
 # TODO: Описать интерфейс шага модерации.
 from abc import ABC, abstractmethod
-from typing import Optional
+from typing import Optional, Union
+
+from services.message_service.domain.moderation import ModerationWarning, ModerationReason
 
 
 # задает только шаблон для других классов
 class ModerationStep(ABC):
     @abstractmethod
-    async def process(self, text: str) -> Optional[str]:
+    async def process(self, text: str) -> Optional[Union[ModerationWarning, ModerationReason]]:
         pass
