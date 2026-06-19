b50461468f47c5c6ec502f5a9455c1ed3a977899
ReeSkaaa <160928530+ReeSkaaa@users.noreply.github.com>
2026-06-19 18:19:45 +0300

    feature(message-service-moderation_module): add pii.py

--- services/message_service/moderation_module/steps/pii.py
+++ services/message_service/moderation_module/steps/pii.py
@@ -1,1 +1,16 @@
 # TODO: Реализовать шаг поиска персональных данных.
+import re
+from services.message_service.moderation_module.step import ModerationStep
+from services.message_service.domain.moderation import ModerationWarning
+
+
+class PIIStep(ModerationStep):
+    TEL_REG = r"(\+7|8|7)[\s\-]?\(?\d{3}\)?[\s\-]?\d{3}[\s\-]?\d{2}[\s\-]?\d{2}"
+    EMAIL_REG = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+
+    async def process(self, text: str):
+        if re.search(self.TEL_REG, text):
+            return ModerationWarning(code="PII_PHONE", message="Phone number is detected")
+        if re.search(self.EMAIL_REG, text):
+            return ModerationWarning(code="PII_EMAIL", message="Email is detected")
+        return None
