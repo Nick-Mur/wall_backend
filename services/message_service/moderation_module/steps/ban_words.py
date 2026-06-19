7fbfd5696dcca46b7905bd6dc6d9999353805ecf
ReeSkaaa <160928530+ReeSkaaa@users.noreply.github.com>
2026-06-19 16:23:28 +0300

    feature(message-service-moderation_module-steps): add ban_words.py

--- services/message_service/moderation_module/steps/ban_words.py
+++ services/message_service/moderation_module/steps/ban_words.py
@@ -1,1 +1,14 @@
 # TODO: Реализовать шаг проверки запрещённых слов.
+from services.message_service.moderation_module.step import ModerationStep
+
+
+class BanWordsStep(ModerationStep):
+    def __init__(self, forbidden_words: list):
+        self.forbidden_words = forbidden_words
+
+    async def process(self, text: str) -> str | None:
+        low_text = text.lower()
+        for word in self.forbidden_words:
+            if word in low_text:
+                return f"Banned word detected: {word}"
+        return None
