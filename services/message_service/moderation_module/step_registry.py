88b660f91ef19a43a715e25e72180ec429783632
ReeSkaaa <160928530+ReeSkaaa@users.noreply.github.com>
2026-06-19 20:09:12 +0300

    feature(message-service-moderation_module): add step_registry.py

--- services/message_service/moderation_module/step_registry.py
+++ services/message_service/moderation_module/step_registry.py
@@ -1,1 +1,22 @@
 # TODO: Реализовать registry/factory шагов модерации.
+"""Создание шагов по строковому названию"""
+from typing import Dict, Type, Any
+from .step import ModerationStep
+from .steps.ban_words import BanWordsStep
+from .steps.pii import PIIStep
+from .steps.normalization import NormalizationStep
+
+
+class RegistryStep:
+    _steps: Dict[str, Type[ModerationStep]] = {
+        "ban_words": BanWordsStep,
+        "pii": PIIStep,
+        "normalization": NormalizationStep
+    }
+
+    @classmethod
+    def create_step(cls, name: str, **kwargs):
+        step_class = cls._steps.get(name)
+        if not step_class:
+            raise ValueError(f"Uknown step: {name}")
+        return step_class(**kwargs)
