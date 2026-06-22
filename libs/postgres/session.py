324ad6e64890bf24adb0dc0ebb290253310371a0
ReeSkaaa <160928530+ReeSkaaa@users.noreply.github.com>
2026-06-22 18:11:39 +0300

    feature(libs-postgres): add session.py

--- libs/postgres/session.py
+++ libs/postgres/session.py
@@ -1,1 +1,39 @@
 # TODO: Реализовать создание engine/session и Unit of Work при необходимости.
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from typing import AsyncGenerator
+
+DATABASE_URL = "postgresql+asyncpg://user:password@localhost:5432/the_wall"
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+AsyncSessionFactory = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionFactory() as session:
+        yield session
+
+
+class UnitOfWork:
+
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        if exc_type:
+            await self.session.rollback()
+        else:
+            await self.session.commit()
+        await self.session.close()
+
+    async def commit(self):
+        await self.session.commit()
+
+    async def rollback(self):
+        await self.session.rollback()
