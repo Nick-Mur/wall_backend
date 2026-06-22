193fe63faff1766eda73e4996555403cede75cda
ReeSkaaa <160928530+ReeSkaaa@users.noreply.github.com>
2026-06-22 15:12:05 +0300

    feature(libs-fast-api): add app_factory.py

--- libs/fastapi/app_factory.py
+++ libs/fastapi/app_factory.py
@@ -1,1 +1,25 @@
 # TODO: Реализовать фабрику FastAPI-приложений.
+from fastapi import FastAPI
+from typing import List, Dict, Type, Callable, Optional
+
+from fastapi.exceptions import RequestValidationError
+
+from .exception_handlers import handle_server_error, handle_validation_error
+
+
+def create_app(
+        title: str,
+        routers: Optional[List] = None,
+        exception_handlers: Optional[Dict[Type[Exception], Callable]] = None
+) -> FastAPI:
+    app = FastAPI(title=title)
+    app.add_exception_handler(RequestValidationError, handle_validation_error)
+    app.add_exception_handler(Exception, handle_server_error)
+    if exception_handlers:
+        for exc_class, handler in exception_handlers.items():
+            app.add_exception_handler(exc_class, handler)
+    if routers:
+        for router in routers:
+            app.include_router(router)
+
+    return app
