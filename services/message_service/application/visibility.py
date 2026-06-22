916b65ab0900c7dabed9b5eeccdc6157c4d33c08
ReeSkaaa <160928530+ReeSkaaa@users.noreply.github.com>
2026-06-22 17:33:34 +0300

    fix(message-service-application): visibility.py

--- services/message_service/application/visibility.py
+++ services/message_service/application/visibility.py
@@ -1,1 +1,31 @@
 # TODO: Реализовать сценарии hide, detach, erase.
+from uuid import UUID
+
+class VisibilityError(ValueError):
+    pass
+
+class VisibilityUseCase:
+    def __init__(self, message_repo):
+        self.repo = message_repo
+
+    async def hide(self, message_id: UUID):
+        msg = await self.repo.get_by_id(message_id)
+        if not msg:
+            raise VisibilityError("Message not found")
+        msg.hide()
+        await self.repo.update(msg)
+
+    async def detach(self, message_id: UUID):
+        msg = await self.repo.get_by_id(message_id)
+        if not msg:
+            raise VisibilityError("Message not found")
+        msg.detach()
+        await self.repo.update(msg)
+
+    async def erase(self, message_id: UUID):
+        msg = await self.repo.get_by_id(message_id)
+        if not msg:
+            raise VisibilityError("Message not found")
+        msg.erase()
+        await self.repo.update(msg)
+
