0330d603b42562e8376f25df8291bb34b1bb785a
NeuroGirl <vesus0206@gmail.com>
2026-06-24 17:51:28 +0300

    Расписаны доменные модели для поискового запроса, результата поиска и списка результатов поиска, описана структура таблицы логирования поисковых операций

--- services/search_service/infrastructure/db_models/search_log.py
+++ services/search_service/infrastructure/db_models/search_log.py
@@ -1,1 +1,11 @@
-# TODO: Описать таблицу search_log.
+from libs.postgres.base import Base
+from datetime import datetime
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+
+
+class SearchLogModel(Base):
+    __tablename__ = "search_log"
+    id = Column(UUID(as_uuid=True), primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    search_request = Column(String, nullable=False)
