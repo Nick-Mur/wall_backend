d88d10d5b2a3d5aacbef7f41bd2e466e987eee63
NeuroGirl <vesus0206@gmail.com>
2026-06-25 13:51:04 +0300

    Реализовано логирование поисковых сессий

--- services/search_service/infrastructure/repositories/search_log_repo.py
+++ services/search_service/infrastructure/repositories/search_log_repo.py
@@ -1,1 +1,17 @@
-# TODO: Реализовать репозиторий логов поиска.
+from sqlalchemy.ext.asyncio import AsyncSession
+from services.search_service.infrastructure.db_models.search_log import SearchLogModel
+from services.search_service.domain.search_request import SearchQuery
+from sqlalchemy import func
+
+class SearchLogRepository:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def save_log(self, query: SearchQuery, results_count: int):
+        log_entry = SearchLogModel(
+            query_string=query.query_string,
+            results_count=results_count,
+            created_at=func.now()
+        )
+        self.session.add(log_entry)
+        await self.session.commit()
