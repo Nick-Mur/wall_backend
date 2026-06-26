1d249676c2df895c7bfbc04149a9649e93a656bb
NeuroGirl <vesus0206@gmail.com>
2026-06-26 16:09:51 +0300

    Написан файл main.py для сервиcа поиска

--- services/search_service/main.py
+++ services/search_service/main.py
@@ -1,1 +1,14 @@
-# TODO: Собрать FastAPI-приложение search-service и подключить router.
+import uvicorn
+from libs.fastapi.app_factory import create_app
+from services.search_service.api.router import router as search_router
+from libs.config_loader import load_config
+
+config = load_config("services/search_service/config.json", env_prefix="SEARCH_SERVICE")
+
+app = create_app(
+    title="search_service",
+    routers=[search_router]
+)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8003)
